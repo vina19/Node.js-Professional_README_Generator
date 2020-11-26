44b21e185a56d92d790d9e9bca58228bf5ecb69b
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-11-26 00:03:59 -0600

    Including package.json and created user's input using npm inquirer package

---
+++ index.js
@@ -0,0 +1,52 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your porject title?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: "Please give a short description of your project: ",
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide instructions and ecamples for use: ',
+        },
+        {
+            type: 'input',
+            name: 'constributing',
+            message: 'Please provide quidelines for other developers who want to contribute to your project: ',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What command should be run to run tests?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message : 'Which license should your project have?',
+            choices: ['MIT License', 'GNU AGPLv3', 'Apache License 2.0', 'Boost Software License 1.0'],
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        }
+    ]);
