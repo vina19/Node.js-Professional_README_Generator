0e035c1ed39d20bbb4f4214e849df950ab8381d7
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-12-01 18:49:01 -0600

    Moved the questions from the user to the questions array

--- index.js
+++ index.js
@@ -3,7 +3,61 @@ const fs = require('fs');
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your porject title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please give a short description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions and examples for use:',
+    },
+    {
+        type: 'input',
+        name: 'constributing',
+        message: 'Please provide quidelines for other developers who want to contribute to your project:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message : 'Which license should your project have?',
+        choices: [
+            'MIT License', 
+            'GNU AGPLv3', 
+            'Apache License 2.0', 
+            'Boost Software License 1.0'],
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'githubLink',
+        message: 'What is the link to your GitHub project repository?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    }
 ];
 
 // function to write README file
@@ -21,61 +75,7 @@ init();
 
 inquirer
     .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is your porject title?',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Please give a short description of your project:',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'What are the steps required to install your project?',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Please provide instructions and examples for use:',
-        },
-        {
-            type: 'input',
-            name: 'constributing',
-            message: 'Please provide quidelines for other developers who want to contribute to your project:',
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'What command should be run to run tests?',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message : 'Which license should your project have?',
-            choices: [
-                'MIT License', 
-                'GNU AGPLv3', 
-                'Apache License 2.0', 
-                'Boost Software License 1.0'],
-        },
-        {
-            type: 'input',
-            name: 'username',
-            message: 'What is your GitHub username?',
-        },
-        {
-            type: 'input',
-            name: 'githubLink',
-            message: 'What is the link to your GitHub project repository?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?',
-        }
+        
     ])
     .then((data) => {
 
