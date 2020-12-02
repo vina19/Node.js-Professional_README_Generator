60654bc96f51a241ce526e0e8536f637dca5f764
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-12-01 18:29:39 -0600

    Adding the project develop code requirements

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
+
 inquirer
     .prompt([
         {
