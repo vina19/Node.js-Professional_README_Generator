2770a1dac44947448f16c5308d852ac53521400c
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-12-02 22:42:20 -0600

    Moved the README template to the generateMarkdown.js for project requirement purpose

--- index.js
+++ index.js
@@ -91,31 +91,7 @@ init();
 
     
     
-const generateReadme = (project) =>  
-`# ${project.title}
-### ${generateLicenseBadge(project.license)}
-## Description
-#### ${project.description}
-## Table of Contents
-#### 1. [Installation](##Installation)
-#### 2. [Usage](##Usage)
-#### 3. [License](##License)
-#### 4. [Contributing](##Constributing)
-#### 5. [Tests](##Tests)
-#### 6. [Questions](##Questions)
-## Installation
-#### ${project.installation}
-## Usage
-#### ${project.usage}
-## License
-#### ${project.license} license notice of which license the application is covered under
-## Contributing
-#### ${project.contributing}
-## Tests
-#### ${project.tests}
-## Questions
-#### GitHub profile: [${project.username}](${project.githubLink})
-### Contact me ${project.email} for more additional questions.`;
+
 
 const generateLicenseBadge = (license) => {
    
