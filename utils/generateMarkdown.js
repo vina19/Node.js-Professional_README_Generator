1183d39d8588f1056f407bafd7526357c382794d
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-12-03 19:09:32 -0600

    Fixed the indentation for the return README template result

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,29 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(project) {
   return `# ${project.title}
-        ### ${generateLicenseBadge(project.license)}
-        ## Description
-        #### ${project.description}
-        ## Table of Contents
-        #### 1. [Installation](##Installation)
-        #### 2. [Usage](##Usage)
-        #### 3. [License](##License)
-        #### 4. [Contributing](##Constributing)
-        #### 5. [Tests](##Tests)
-        #### 6. [Questions](##Questions)
-        ## Installation
-        #### ${project.installation}
-        ## Usage
-        #### ${project.usage}
-        ## License
-        #### ${project.license} license notice of which license the application is covered under
-        ## Contributing
-        #### ${project.contributing}
-        ## Tests
-        #### ${project.tests}
-        ## Questions
-        #### GitHub profile: [${project.username}](${project.githubLink})
-        ### Contact me ${project.email} for more additional questions.`
+### ${generateLicenseBadge(project.license)}
+## Description
+#### ${project.description}
+## Table of Contents
+#### 1. [Installation](##Installation)
+#### 2. [Usage](##Usage)
+#### 3. [License](##License)
+#### 4. [Contributing](##Constributing)
+#### 5. [Tests](##Tests)
+#### 6. [Questions](##Questions)
+## Installation
+#### ${project.installation}
+## Usage
+#### ${project.usage}
+## License
+#### This application is covered under ${generateLicenseNotice(project.license)}.
+## Contributing
+#### ${project.contributing}
+## Tests
+#### ${project.tests}
+## Questions
+#### GitHub profile: [${project.username}](${project.githubLink})
+### Contact me ${project.email} for more additional questions.`
 };
 
 const generateLicenseBadge = (license) => {
